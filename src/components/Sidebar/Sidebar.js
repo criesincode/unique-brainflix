f9f8cb85e02a692434cf6a650797da94164ab7a8
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-10-19 10:52:57 -0400

    work on skeletion

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,26 @@
+const Sidebar = () => {
+    return (
+        <section class="sidebar">
+        <p>NEXT VIDEOS</p>
+        <div class="sideobarVideo"> <video></video> A Travel Pro In One Easy Lesson
+            Todd Welch</div>
+        <div class="sidebarVideo"> <video></video> Les Houches The Hidden Gem Of The...
+            Cornelia Blair</div>
+        <div class="sidebarVideo"> <video></video> Travel Health Useful Medical Information...
+            Glen Harper
+            </div>
+        <div class="sidebarVideo"> <video></video> Cheap Airline Tickets Great Ways To Save
+            Emily Harper</div>
+        <div class="sidebarVideo"> <video></video> Take A Romantic Break In A Boutique...
+            Ethan Owen</div>
+        <div class="sidebarVideo"> <video></video> Choose The Perfect Accomodations
+            Lydia Perez</div>
+        <div class="sidebarVideo"> <video></video> Cruising Destination Ideas
+            Timothy Austin</div>
+        <div class="sidebarVideo"> <video></video> Train Travel On Track For Safety
+            Scotty Cramer</div>
+    </section> 
+    )
+}
+
+export { Sidebar };
