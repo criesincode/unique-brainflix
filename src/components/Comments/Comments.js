f399b31c94a1bf5849c9dcef2e12e1696d494c6e
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-10-20 16:01:16 -0400

    work on comments skeleton

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,15 +1,12 @@
+import Comments "./"
+
 const Comments = () => {
     return (
         <section className="comments">
-        <div className="commentsContainer">Micheal Lyons
-            08/09/2021
-            They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.</div>
-        <div className="commentsContainer">Gary Wong 07/15/2021
-            Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!
-            </div>
-        <div className="commentsContainer">Theodore Duncan 07/11/2021
-            How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!</div>     
-    </section>
+        <div className="commentsContainer">
+            <p className="commentsDetail">{video-details.comment}</p>
+        </div> 
+        </section>   
     )
 }
 
