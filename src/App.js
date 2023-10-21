70ef30a0b20d151f9dd48066600a7713f0ebc4a1
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-10-21 11:45:49 -0400

    adjust import structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
+import { useState } from "react"
 import Header from "./components/Header/Header.js"
 import Hero from "./components/Hero/Hero.js"
 import Main from "./components/Main/Main.js"
 import Comments from "./components/Comments/Comments.js"
 import Sidebar from "./components/Sidebar/Sidebar.js"
-import './App.scss';
-import { useState } from "react"
 import videos from "./data/video-details.json"
+import './App.scss';
+
 
 function App() {
   const [currentVideo, setCurrentVideo] = useState (
