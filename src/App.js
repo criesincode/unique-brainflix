41750ec18bef0e4ea542cc3527fb5d33f833deae
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-10-21 16:00:59 -0400

    mapping

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react"
+import { useState } from "react"
 import Header from "./components/Header/Header.js"
 import Hero from "./components/Hero/Hero.js"
 import Main from "./components/Main/Main.js"
 import Comments from "./components/Comments/Comments.js"
 import Sidebar from "./components/Sidebar/Sidebar.js"
+import details from "./data/video-details.json"
 import './App.scss';
 
 function App() {
+  const [currentDetails, setCurrentDetails] = useState (
+    details[0]
+  )
   return (
     <div className="App">
       {/* <Header />
       <Hero /> */}
-      <Main />
+      <Main 
+        title={currentDetails.title}/>
       {/* <Comments />
       <Sidebar /> */}
     </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,27 +1,18 @@
+import "./Main.scss"
+
 const Main = () => {
     return (
-        <main className="main">
-            <p className="mainTitle">BMX Rampage: 2021 Highlights</p>
-            <div className="mainDetails">
-                <p className="mainInfo">On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title</p>
-                <p className="mainInfo">3 Comments</p>
-            </div>
-            <div className="mainInfoCommentSection">
-                <h2 className="mainCommentSectionText">Join the Conversation</h2>
-            </div>
-            <div className="mainCommentContainer">
-                <img className="mainCommentImage" src="./assets/images/Mohan-muruge.jpg"/>
-                    <form id="mainCommentForm">
-                        <div className="mainCommentInput">
-                            <h5 className="mainCommentInputText">COMMENT</h5>
-                            <label className="comment" for="comment"></label>
-                            <input type="comment" name="comment" id="comment" placeholder="Add a new comment"/>
-                            <br />
-                            <button>COMMENT</button>
-                        </div>    
-                    </form>     
-            </div>  
-        </main>  
+        <>
+        {details.map(details => {
+            return (
+                <main className="main">
+                    <div className="mainTitleContainer">
+                        <p className="mainTitle">{details.title}</p>
+                    </div>
+                </main>     
+            )                  
+        })}    
+      </>  
     )
 }
 
