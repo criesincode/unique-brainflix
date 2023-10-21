2823c36f893a65b3bdb05f2f293978b65b5cb334
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-10-21 16:21:40 -0400

    nothing special

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,17 @@
 import "./Main.scss"
 
-const Main = () => {
+const Main = ({}) => {
     return (
         <>
         {details.map(details => {
             return (
+                <>
                 <main className="main">
                     <div className="mainTitleContainer">
                         <p className="mainTitle">{details.title}</p>
                     </div>
-                </main>     
+                </main> 
+                </>    
             )                  
         })}    
       </>  
