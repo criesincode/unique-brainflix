f7719750695d21d26edb5bfb99b36263cce7ed98
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-10-22 23:41:22 -0400

    merge process

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Hero videoDetails={videoDetails}/>
+      <Hero videoDetails={heroVideo}/>
       <Main />
       <Comments />
       <Sidebar /> 
