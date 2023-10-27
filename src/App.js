6fb86e74b4c9ce74c59d7343b36b5aea513c21e0
Unique Dismuke <uniquedismuke@Uniques-MacBook-Air.local>
2023-10-27 18:05:49 -0400

    got help from nick in terms of my routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.js";
-import Hero from "./components/Hero/Hero.js";
-import Main from "./components/Main/Main.js";
-import Comments from "./components/Comments/Comments.js";
-import Sidebar from "./components/Sidebar/Sidebar.js";
 import HomePage from "./pages/HomePage/HomePage.js";
 import VideoUploadPage from "./pages/VideoUploadPage/VideoUploadPage.js"
-import VideoDetailsPage from "./pages/VideoDetailsPage/VideoDetailsPage.js"
 import "./App.scss"
 
 
@@ -19,8 +14,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/videos/:videoId" element={<VideoDetailsPage />} />
-          {/* <Route path="/upload" element={<VideoUploadPage />} /> */}
+          <Route path="/videos/:videoId" element={<HomePage />} />
+          <Route path="/upload" element={<VideoUploadPage />} />
         </Routes>
       </Router>
       
