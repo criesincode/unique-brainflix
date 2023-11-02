d0484e30352ea952eeaea95bea6776b14fc71d2f
Unique Dismuke <uniquedismuke@uniques-air.mynetworksettings.com>
2023-11-02 16:29:36 -0400

    add image name to newVideo object

--- src/pages/VideoUploadPage/VideoUploadPage.js
+++ src/pages/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,34 @@
 import "./VideoUploadPage.scss"
 import UploadVideo from "../../assets/images/Upload-video-preview.jpg"
 import { Link } from "react-router-dom"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 const VideoUploadPage = () => {
+    const navigate = useNavigate()
     const uploadAlert = () => {
         window.alert("Video upload was successful! Return to homepage.")
+    } 
+    const handleFormSubmission = (event) => {
+        event.preventDefault();  
+        console.log(event.target.title.value)
+        console.log(event.target.description.value)
+    const newVideo = {
+        title: event.target.title.value,
+        description: event.target.description.value,
+        image: "Upload-video-preview.jpg"
+    } 
+    const postVideo = async () => {
+        try {
+          const response = await axios.post(`http://localhost:8080/videos`, newVideo);
+          navigate("/")
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      postVideo()   
     }
+    
     return (
         <>
             <div className="title__container">
@@ -17,7 +40,7 @@ const VideoUploadPage = () => {
                     <p className="upload__thumbnail--title">VIDEO THUMBNAIL</p>
                     <img className="upload__image" src={UploadVideo} alt="riding bike upload" />
                 </div>
-                <form>
+                <form onSubmit={handleFormSubmission}>
                     <p className="form__text">TITLE YOUR VIDEO</p>
                     <div className="form__input">
                         <label className="title" htmlFor="title"></label>
@@ -29,18 +52,15 @@ const VideoUploadPage = () => {
                         <input type="text" name="description" id="description" placeholder="Add a description to your video" />
                         <br />
                         <div className="button__container">
-                            <Link to="/">
-                                <button className="button" onSubmit={uploadAlert}>PUBLISH</button>
-                            </Link>
+                            {/* <Link to="/"> */}
+                                <button className="button" type="submit">PUBLISH</button>
+                            {/* </Link> */}
                             <Link to="/">
                                 <button className="button__b">CANCEL</button>
                             </Link>
                         </div>
                     </div>
                 </form>
-
-                {/* <Link to="/" className="button">
-            </Link> */}
 
             </section>
         </>
